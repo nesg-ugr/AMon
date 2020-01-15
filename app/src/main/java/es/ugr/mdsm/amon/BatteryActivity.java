dd3ed01ddf5847cff1737217084369cedb07397e
jmnoguera <jmnoguerac@ugr.es>
2020-01-15 11:49:50 +0100

    Changed the battery optimization activity to show the information from DKMA.

--- app/src/main/java/es/ugr/mdsm/amon/BatteryActivity.java
+++ app/src/main/java/es/ugr/mdsm/amon/BatteryActivity.java
@@ -1,10 +1,12 @@
 package es.ugr.mdsm.amon;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.Uri;
@@ -12,11 +14,17 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.judemanutd.autostarter.AutoStartPermissionHelper;
+
 import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+import dev.doubledot.doki.views.DokiContentView;
 
 
 public class BatteryActivity extends AppCompatActivity {
@@ -25,6 +33,7 @@ public class BatteryActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private TextView noStepText;
+    private TextView stillStepText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,7 @@ public class BatteryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_battery);
 
         noStepText = findViewById(R.id.noStep);
+        stillStepText = findViewById(R.id.stillStep);
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
 
@@ -57,8 +67,10 @@ public class BatteryActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         if (batterySteps.size() == 0){
             noStepText.setVisibility(View.VISIBLE);
+            stillStepText.setVisibility(View.GONE);
         }else{
             noStepText.setVisibility(View.GONE);
+            stillStepText.setVisibility(View.VISIBLE);
         }
     }
 
@@ -105,6 +117,47 @@ public class BatteryActivity extends AppCompatActivity {
             }
         }
 
+        if(AutoStartPermissionHelper.getInstance().isAutoStartPermissionAvailable(this)){
+            batterySteps.add(new BatteryStep(
+                    getString(R.string.step, ++initialStep),
+                    getString(R.string.step_description_autostart),
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            AutoStartPermissionHelper.getInstance().getAutoStartPermission(BatteryActivity.this);
+                        }
+                    })
+            );
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(BatteryActivity.this);
+        View view = View.inflate(BatteryActivity.this, R.layout.activity_doki, null);
+        DokiContentView doki = view.findViewById(R.id.doki_content);
+        if(doki!=null) {
+            doki.setButtonsVisibility(false);
+            // workaround since the library doesn't use the @JvmOverloads annotation
+            doki.loadContent(Build.MANUFACTURER.toLowerCase().replace(" ", "-"));
+        }
+        builder.setView(view);
+        builder.setPositiveButton(R.string.doki_close, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+
+        batterySteps.add(new BatteryStep(
+                getString(R.string.step, ++initialStep),
+                getString(R.string.step_description_more),
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.show();
+                    }
+                }
+        ));
+        /*
         String manufacturer = Build.MANUFACTURER;
 
         if(manufacturer.equalsIgnoreCase("Xiaomi")){
@@ -140,6 +193,7 @@ public class BatteryActivity extends AppCompatActivity {
                     getString(R.string.step_description_lock)
             ));
         }
+        */
 
         return batterySteps;
     }
